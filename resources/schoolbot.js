5ce25a200675d82790915a98f3605da64035bbba
Ben Smith <thegenieoftruth@gmail.com>
2016-11-13 20:54:07 -0500

    Update schoolbot.js

--- resources/schoolbot.js
+++ resources/schoolbot.js
@@ -1,1 +1,47 @@
+function clickify(targetNode){
+if (targetNode) {
+//--- Simulate a natural mouse-click sequence.
+triggerMouseEvent (targetNode, "mouseover");
+triggerMouseEvent (targetNode, "mousedown");
+triggerMouseEvent (targetNode, "mouseup");
+triggerMouseEvent (targetNode, "click");
+}
+else{
+console.log ("*** Target node not found!");
+}}
+function triggerMouseEvent (node, eventType) {
+var clickEvent = document.createEvent ('MouseEvents');
+clickEvent.initEvent (eventType, true, true);
+node.dispatchEvent (clickEvent);
+}
+setInterval(function(){
 
+
+var arr = "oh poop nuggets Mr. Trump has become president".split(" ")
+function shuffle(array) {
+var currentIndex = array.length, temporaryValue, randomIndex;
+
+// While there remain elements to shuffle...
+while (0 !== currentIndex) {
+
+ // Pick a remaining element...
+ randomIndex = Math.floor(Math.random() * currentIndex);
+ currentIndex -= 1;
+
+ // And swap it with the current element.
+ temporaryValue = array[currentIndex];
+ array[currentIndex] = array[randomIndex];
+ array[randomIndex] = temporaryValue;
+}
+
+return array;
+}
+arr = shuffle(arr)
+arr[0] = arr[0].substring(0,1).toUpperCase()+ arr[0].substring(1,arr[0].length).toLowerCase()
+arr[arr.length-1] = arr[arr.length-1]+"!"
+arr = arr.join(" ")
+
+document.getElementById("edit-submit").disabled  = false
+document.getElementById("edit-comment").value = arr
+clickify(document.getElementById("edit-submit"))
+},1000)
