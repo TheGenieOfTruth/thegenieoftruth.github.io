08712df6df21360c088fde3d292673e95254ee9e
Ben Smith <thegenieoftruth@gmail.com>
2016-11-14 11:08:33 -0500

    Update build.js

--- resources/build.js
+++ resources/build.js
@@ -1,7 +1,7 @@
 function build(name,raw,description){
-var a = `<h4>`+name+`</h4><br>`
+var a = '<h4>'+name+'</h4><br>'
 if(description!=undefined){
   a += "<p>"+description+"</p>"
 }
-return a + <a href = "`+raw+`">raw</a>`
+document.getElementById("content").innerHTML += a + `<a href = "`+raw+`">raw</a>`
 }
