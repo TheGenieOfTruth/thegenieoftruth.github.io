bbac2aa1da774b93257ace8f05bdf1b4c80cba07
TheGenieOfTruth <thegenieoftruth@gmail.com>
2017-02-03 15:16:27 -0500

    Shizz with gulpfile

---
+++ gulp/app/js/shoop.js
@@ -0,0 +1,1 @@
+console.log("Hello there, I am shoop.js!");
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref')
 var browserSync = require('browser-sync').create();
 gulp.task('browserSync',function(){
     browserSync.init({
         server:{
             baseDir: 'app'
         },
+        open:false
     })
 });
 gulp.task('sass', function(){
@@ -18,5 +20,12 @@ gulp.task('sass', function(){
 });
 gulp.task('watch', ['browserSync','sass'], function(){
     gulp.watch('app/**/*.scss',['sass']);
+    gulp.watch('app/*.html',browserSync.reload)
+    gulp.watch('app/js/**/*.js',browserSync.reload)
     // Other stoof
+});
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
 });
