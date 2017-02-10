b17418071c3b1c03a776984ff416712a2f0b2922
thegenieoftruth <thegenieoftruth@gmail.com>
2017-02-10 15:09:45 -0500

    Gulp almost finished

--- gulpfile.js
+++ gulpfile.js
@@ -12,21 +12,19 @@ var runSequence = require('run-sequence');
 var autoprefixer = require('gulp-autoprefixer');
 var jade = require('gulp-jade');
 var coffee = require('gulp-coffee');
-var loc = ["/gulp","/harmonicraft"];
+var replace = require('gulp-replace');
+var loc = ["gulp/","harmonicraft/"];
 var cwd = "";
 
-function def(cb,x){
+function def(x,callback){
   cwd = x
-  runSequence('jade','coffee',
-    ['sass', 'useref', 'fonts'],
-    cb
-  )
+  runSequence('jade','coffee','sass', 'useref', 'fonts','itr',callback);
 }
 gulp.task('coffee',function(){
   return gulp.src(cwd + 'app/coffee/**/*.coffee')
     .pipe(coffee())
     .pipe(gulp.dest(cwd + 'app/js'));
-})
+});
 gulp.task('jade', function() {
     return gulp.src(cwd + 'app/**/*.jade')
         .pipe(jade({ pretty: true })) // pip to jade plugin
@@ -35,7 +33,7 @@ gulp.task('jade', function() {
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: cwd + 'app'
+            baseDir: cwd
         },
         open: false
     });
@@ -81,14 +79,31 @@ gulp.task('clean', function() {
     return cache.clearAll(cb);
   });
 });
+gulp.task('itr',function(){
+        return gulp.src(cwd+'dist/index.html')
+        .pipe(replace('href="css/','href="dist/css/'))
+        .pipe(replace('src="images/','src="dist/images/'))
+        .pipe(replace('src="js/','src="dist/js/'))
+        .pipe(gulp.dest(cwd));
+})
 gulp.task('build', function (callback) {
   runSequence('jade','coffee',
     ['sass', 'useref', 'images', 'fonts'],
     callback
-  )
+);
 });
-gulp.task('default', function (callback) {
-loc.forEach(function(val){
-    def(callback,val)
-})
+gulp.task('default', function () {
+    var ct = 0
+function loop() {
+    if (ct < loc.length) {
+        console.log("\x1b[34mRunning tasks under directory: " + loc[ct]+"\x1b[0m")
+        def(loc[ct],loop)
+        ct++;
+    } else{
+        console.log("")
+        console.log("\x1b[32mBUILD PASSED ON TASK 'DEFAULT' :)\x1b[0m")
+        console.log("Yay! If the program made it this far, there were no errors, or there were, and there's gonna be hella debugging party for you.")
+    }
+}
+loop()
 });
