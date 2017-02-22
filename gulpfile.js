6767f85d3a54e7f21d26f89103e8b6058b160b9c
thegenieoftruth <thegenieoftruth@gmail.com>
2017-02-21 22:00:17 -0500

    That worked well. TOO well.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 var autoprefixer = require('gulp-autoprefixer');
-var jade = require('gulp-jade');
+var pug = require('gulp-pug');
 var coffee = require('gulp-coffee');
 var replace = require('gulp-replace');
 var fs = require('fs');
@@ -23,18 +23,18 @@ var cwd = "";
 var htmlprettify = require('gulp-html-beautify');
 var jsprettify = require('gulp-jsbeautifier');
 var cssprettify = require('gulp-cssbeautify');
-var jadeprettify = require('gulp-pug-beautify');
+var pugprettify = require('gulp-pug-beautify');
 function
 def(x, callback) {
 	cwd = x;
-		runSequence('clean','beautify-css','beautify-js','beautify-html', 'images', 'jade', 'coffee', 'copy-base', 'sass', 'useref', 'fonts', 'itr', 'copy-css', 'copy-js', callback);
+		runSequence('clean','beautify-css','beautify-js','beautify-html','beautify-pug', 'images', 'pug', 'coffee', 'copy-base', 'sass', 'useref', 'fonts', 'itr', 'copy-css', 'copy-js', callback);
 }
 gulp.task('coffee', function() {
 	return gulp.src(cwd + 'app/coffee/**/*.coffee')
 		.pipe(coffee())
 		.pipe(gulp.dest(cwd + 'app/js'));
 });
-gulp.task('jade', function() {
+gulp.task('pug', function() {
 	//locals.root, used for building via relative paths. NEVER USE IN FRONTEND OR YOU WILL DIE, CHILD
 	var data = {}
 	data.times = cwd.split("/")
@@ -43,8 +43,8 @@ gulp.task('jade', function() {
 	for (i = 0; i < data.times; i++) {
 		data.root += "../";
 	}
-	data.a = data.root + "base/jade/"
-	glob.sync("base/jade/*.jade")
+	data.a = data.root + "base/pug/"
+	glob.sync("base/pug/*.pug")
 		.forEach(function(val) {
 			var a = val.split("/");
 		a = a[a.length - 1];
@@ -52,12 +52,16 @@ gulp.task('jade', function() {
 			data[a] = val
 		});
 
-	data.a = require("jade").renderFile
-	return gulp.src(cwd + 'app/**/*.jade')
-		.pipe(jade({
+    data.config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+    data.a = function(){
+        arguments[1] = arguments[1] === undefined || arguments[1] === null ? {} : arguments[1]
+        return require("pug").renderFile(arguments[0],Object.assign(arguments[1],data))
+    }
+	return gulp.src(cwd + 'app/**/*.pug')
+		.pipe(pug({
             pretty:true,
 			"data": data
-		})) // pip to jade plugin
+		})) // pip to pug plugin
 		.pipe(gulp.dest(cwd + 'app')); // tell gulp our output folder
 });
 gulp.task('browserSync', function() {
@@ -79,13 +83,13 @@ gulp.task('watch', function(callback) {
 			cwd = argv.a != undefined ? argv.a : ""
 			browserSync.reload()
 			gulp.watch(cwd + 'app/coffee/**/*.coffee', ['coffee']); //reload via javascript change
-			gulp.watch(cwd + 'app/*.jade', ['jade']); //reload via HTML change
+			gulp.watch(cwd + 'app/*.pug', ['pug']); //reload via HTML change
 			gulp.watch(cwd + 'app/**/*.scss', ['sass']); //reload via CSS change
 			gulp.watch(cwd + 'app/*.html', browserSync.reload); //reload
 			gulp.watch(cwd + 'app/js/**/*.js', browserSync.reload); //reload
 			gulp.watch(cwd + 'app/css/**/*.css', browserSync.reload); //reload
-			gulp.watch('base/scss/*.scss',['copy-base','jade']);
-			gulp.watch("base/jade/*.jade",["jade"]);
+			gulp.watch('base/scss/*.scss',['copy-base','pug']);
+			gulp.watch("base/pug/*.pug",["pug"]);
 		})
 		// Other stoof
 });
@@ -119,9 +123,9 @@ gulp.task('itr', function() {
 		.pipe(replace(/<script>(.*)(src="js\/)(.*)<\/script>/g, '<script>$1src="dist/js/$3</script>'))
 		.pipe(gulp.dest(cwd));
 });
-//Jade template clean
+//pug template clean
 gulp.task('build', ['default'], function() {});
-gulp.task('default',function(callback) {
+gulp.task('default',['beautify-root-pug'],function(callback) {
 	var ct = 0
 
 	function loop() {
@@ -165,13 +169,21 @@ gulp.task('beautify-html', function(){
 		.pipe(htmlprettify())
 		.pipe(gulp.dest(cwd + "app"))
 });
-gulp.task('beautify-jade',function(){
-	return gulp.src(cwd + 'app/**/*.jade')
-		.pipe(jadeprettify({
-			tab_size:1,
+gulp.task('beautify-pug',function(){
+	return gulp.src(cwd + 'app/**/*.pug')
+		.pipe(pugprettify({
+			tab_size:4,
 			fill_tab:false
 		}))
 		.pipe(gulp.dest(cwd + "app"))
+})
+gulp.task('beautify-root-pug',function(){
+    return gulp.src('base/pug/**/*.pug')
+		.pipe(pugprettify({
+			tab_size:4,
+			fill_tab:false
+		}))
+		.pipe(gulp.dest("base/pug"))
 })
 gulp.task('help', function() {
 	console.log("watch:")
