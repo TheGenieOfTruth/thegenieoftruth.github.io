b00f838c035c5a7862c22dd96726361070c4e9ca
thegenieoftruth <thegenieoftruth@gmail.com>
2017-04-14 12:11:39 -0400

    Slidey

--- app/js/thing.js
+++ app/js/thing.js
@@ -5,24 +5,24 @@ $(".regular").slick({
     autoplaySpeed: 5000
 });
 var elem = $("<div></div>")
-elem.css("background","white")
-elem.css("padding","2px 0px")
-elem.css("margin","30px 0px")
-elem.css("width","0%")
+elem.css("background", "white")
+elem.css("padding", "2px 0px")
+elem.css("margin", "30px 0px")
+elem.css("width", "0%")
 var n1 = elem.clone()
 var n2 = elem.clone()
-n2.css("margin-left","100%")
+n2.css("margin-left", "100%")
 $("#header-target").prepend(n1)
 $("#header-target").append(n2)
 n1.animate({
-	"width":"80%",
-	"margin-left":"10%"
-},1000)
+    "width": "80%",
+    "margin-left": "10%"
+}, 1000)
 n2.animate({
-	"width":"80%",
-	"margin-left":"10%"
-},1000)
-setTimeout(function(){
-	outport.start()
-	outport.explode()
-},800)
+    "width": "80%",
+    "margin-left": "10%"
+}, 1000)
+setTimeout(function() {
+    outport.start()
+    outport.explode()
+}, 800)
